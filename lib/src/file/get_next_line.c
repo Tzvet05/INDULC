cf48af5930e82f456bb30ab5f70f6e33e05f77b0
Tzvetan Trave <tzvetan.trave@abadcafe.org>
2025-03-19 01:17:59 +0100

    error during reading now sets *line to NULL

--- lib/src/file/get_next_line.c
+++ lib/src/file/get_next_line.c
@@ -1,22 +1,20 @@
 #include <stdlib.h>
 #include "file.h"
 
-bool	get_next_line(t_file* file, char** line_ptr)
+bool	get_next_line(t_file* file, char** line)
 {
-	char*	line = NULL;
-	size_t	null = 0;
-	ssize_t	n_read = getline(&line, &null, file->stream);
+	size_t	_ = 0;
+	ssize_t	n_read = getline(line, &_, file->stream);
 	if (n_read == -1 && ferror(file->stream) == 1)
 	{
-		free(line);
+		free(*line);
+		*line = NULL;
 		return (1);
 	}
 	else if (n_read == -1 && feof(file->stream) == 1)
 	{
-		free(line);
-		*line_ptr = NULL;
+		free(*line);
+		*line = NULL;
 	}
-	else
-		*line_ptr = line;
 	return (0);
 }
