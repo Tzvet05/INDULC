5ca37efbfc406d4fe8557614909f8df652762f9f
Tzvetan Trave <tzvetan.trave@abadcafe.org>
2025-10-06 00:34:49 +0200

    cleaned duplicate code and if statements

--- lib/src/file/get_next_line.c
+++ lib/src/file/get_next_line.c
@@ -3,18 +3,15 @@
 
 bool	get_next_line(t_file* file, char** line)
 {
+	*line = NULL;
 	size_t	_ = 0;
 	ssize_t	n_read = getline(line, &_, file->stream);
-	if (n_read == -1 && ferror(file->stream) == 1)
+	if (n_read == -1)
 	{
 		free(*line);
 		*line = NULL;
-		return (1);
-	}
-	else if (n_read == -1 && feof(file->stream) == 1)
-	{
-		free(*line);
-		*line = NULL;
+		if (ferror(file->stream) == 1)
+			return (1);
 	}
 	return (0);
 }
