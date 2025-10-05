6cbaac432c9179ca718551abf6594f0e08347ca8
Tzvetan Trave <tzvetan.trave@abadcafe.org>
2025-10-06 00:33:04 +0200

    removed useless else statements

--- lib/src/nbr/get_charset.c
+++ lib/src/nbr/get_charset.c
@@ -5,10 +5,9 @@ char*	get_charset(char* str)
 {
 	if (strncmp(str, HEX_PREFIX, strlen(HEX_PREFIX)) == 0)
 		return (HEX_CHARSET);
-	else if (strncmp(str, OCT_PREFIX, strlen(OCT_PREFIX)) == 0)
+	if (strncmp(str, OCT_PREFIX, strlen(OCT_PREFIX)) == 0)
 		return (OCT_CHARSET);
-	else if (strncmp(str, BIN_PREFIX, strlen(BIN_PREFIX)) == 0)
+	if (strncmp(str, BIN_PREFIX, strlen(BIN_PREFIX)) == 0)
 		return (BIN_CHARSET);
-	else
-		return (DEC_CHARSET);
+	return (DEC_CHARSET);
 }
--- src/machine_code/get_operand.c
+++ src/machine_code/get_operand.c
@@ -9,6 +9,5 @@ ssize_t	get_immediate_operand(t_lst* symbol_table, t_token* token)
 	t_lst*	label = lst_find(symbol_table, token->str, cmp_label);
 	if (label != NULL)
 		return ((ssize_t)((t_label *)label->content)->line);
-	else
-		return (get_number(token->str));
+	return (get_number(token->str));
 }
